5ec1cce605d078bd1aea9f3d708e9e7f9087e340
André Kloster <a.kloster@businessangels.de>
2024-05-28 18:28:44 +0200

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,11 +8,10 @@ vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true, silent = true })
 -- Custom Schema for Kubernetes
 vim.api.nvim_set_keymap("n", "<C-k>s", ':lua require("user.schema").init()<CR>', { noremap = true, silent = true })
 
--- Custom Schema for Kubernetes
--- vim.api.nvim_set_keymap("n", "sfa", ":set ft=yaml.ansible<CR>", { noremap = true, silent = true })
+-- Set filetype for Ansible
 vim.api.nvim_set_keymap(
-  "n",
-  "<C-k>a",
-  ":set ft=yaml.ansible<CR>:lua print('Set filetype: ansible')<CR>",
-  { noremap = true, silent = true }
+	"n",
+	"<C-k>a",
+	":set ft=yaml.ansible<CR>:lua print('Set filetype: ansible')<CR>",
+	{ noremap = true, silent = true }
 )

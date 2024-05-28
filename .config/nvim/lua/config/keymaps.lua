50e066e866ef42e4d1413b16a114506d6d5545ab
André Kloster <a.kloster@businessangels.de>
2024-05-28 18:26:14 +0200

    changed neovim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,4 +6,13 @@
 vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true, silent = true })
 
 -- Custom Schema for Kubernetes
-vim.api.nvim_set_keymap("n", "kcs", ':lua require("user.schema").init()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-k>s", ':lua require("user.schema").init()<CR>', { noremap = true, silent = true })
+
+-- Custom Schema for Kubernetes
+-- vim.api.nvim_set_keymap("n", "sfa", ":set ft=yaml.ansible<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-k>a",
+  ":set ft=yaml.ansible<CR>:lua print('Set filetype: ansible')<CR>",
+  { noremap = true, silent = true }
+)
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,15 +3,16 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        yamlls = {},
+        ansiblels = {},
+        docker_compose_language_service = {},
+        dockerls = {},
         helm_ls = {},
         ruff_lsp = {
           handlers = {
             ["textDocument/publishDiagnostics"] = function() end,
           },
         },
-        dockerls = {},
-        docker_compose_language_service = {},
+        yamlls = {},
       },
     },
   },

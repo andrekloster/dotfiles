2223eda08011e5fa68b553076862688a0041db57
andrekloster <kloster.andre@gmail.com>
2024-09-11 16:22:23 +0200

    Remove yamlls from nvim config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,19 +1,18 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        ansiblels = {},
-        docker_compose_language_service = {},
-        dockerls = {},
-        helm_ls = {},
-        ruff_lsp = {
-          handlers = {
-            ["textDocument/publishDiagnostics"] = function() end,
-          },
-        },
-        yamlls = {},
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				ansiblels = {},
+				docker_compose_language_service = {},
+				dockerls = {},
+				helm_ls = {},
+				ruff_lsp = {
+					handlers = {
+						["textDocument/publishDiagnostics"] = function() end,
+					},
+				},
+			},
+		},
+	},
 }

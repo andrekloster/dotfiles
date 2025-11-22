7932b80956a9091d22fbd7a5961794a4c5945378
andrekloster <andre.kloster@domus-server.de>
2025-11-23 00:09:04 +0100

    Update bufferline and disabled plugins

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,9 +1,9 @@
 return {
-  "akinsho/bufferline.nvim",
-  keys = {
-    { "<leader>bh", "<cmd>BufferLineMovePrev<cr>", desc = "Move Prev Buffer" },
-    { "<leader>bl", "<cmd>BufferLineMoveNext<cr>", desc = "Move Next Buffer" },
-    { "<Tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Prev Buffer" },
-    { "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Next Buffer" },
-  },
+	"akinsho/bufferline.nvim",
+	keys = {
+		{ "<leader>bh", "<cmd>BufferLineMovePrev<cr>", desc = "Move Prev Buffer" },
+		{ "<leader>bl", "<cmd>BufferLineMoveNext<cr>", desc = "Move Next Buffer" },
+		{ "<Tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Prev Buffer" },
+		{ "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Next Buffer" },
+	},
 }
